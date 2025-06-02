82166f6e4913898afba272eba9f6c0853c66dd8d
Ananthi2510 <ananthikalishwaran2002@gmail.com>
2025-06-02 15:37:37 +0530

    Create full_name.py

---
+++ inputs & outputs - basics/full_name.py
@@ -0,0 +1,6 @@
+# Read first name and last name from user input
+first_name = input()
+last_name = input()
+
+# Display full name by joining first and last name with a space
+print(first_name + " " + last_name)
