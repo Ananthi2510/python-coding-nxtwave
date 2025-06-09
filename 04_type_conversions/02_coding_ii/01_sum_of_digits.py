9e6a36cc742dedd5f7f838276232c4d85fc6a03c
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-09 18:58:46 +0530

    Create 01_sum_of_digits.py

---
+++ 04_type_conversions/02_coding_ii/01_sum_of_digits.py
@@ -0,0 +1,9 @@
+#Get a 3 digit input
+word = input()
+
+first_digit = int(word[0])
+second_digit = int(word[1])
+third_digit = int(word[2])
+
+result = first_digit + second_digit + third_digit
+print(result)
