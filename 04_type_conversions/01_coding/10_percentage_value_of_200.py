8c3c6215a925da706d7fd027f7d7cae64c314dee
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-09 18:24:46 +0530

    Create 10_percentage_value_of_200.py

---
+++ 04_type_conversions/01_coding/10_percentage_value_of_200.py
@@ -0,0 +1,8 @@
+#given a percentage value
+
+percentage = input()
+percentage = int(percentage)
+
+#To find the percentage of 200 that corresponds to the input value
+value = (percentage / 100) * 200
+print(value)
