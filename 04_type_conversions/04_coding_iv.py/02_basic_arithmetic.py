542a4f3f6bac5b786aafab13a987143f9556a079
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-10 13:28:51 +0530

    Create 02_basic_arithmetic.py

---
+++ 04_type_conversions/04_coding_iv.py/02_basic_arithmetic.py
@@ -0,0 +1,12 @@
+#get two integers
+A = int(input())
+B = int(input())
+
+#sum of the given integers
+print(A + B)
+
+#subtraction of the given integers
+print(A - B)
+
+#product of the given integers
+print(A * B)
