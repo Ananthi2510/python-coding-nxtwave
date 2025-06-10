62c13d4800b2ec83140a526bea14ee2aa940f156
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-10 13:42:26 +0530

    Create 04_area_perimeter_square.py

---
+++ 04_type_conversions/04_coding_iv.py/04_area_perimeter_square.py
@@ -0,0 +1,8 @@
+#lenght of the side of the square 
+side = int(input())
+
+area = side * side
+perimeter = 4 * side
+
+print("Area of the square is: " + str(area))
+print("Perimeter of the square is: " + str(perimeter))
