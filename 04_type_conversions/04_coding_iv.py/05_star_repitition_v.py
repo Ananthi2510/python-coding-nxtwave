19497bc9307157d2da1cb3bebbb61db46e5f32ce
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-10 13:49:37 +0530

    Create 05_star_repitition_v.py

---
+++ 04_type_conversions/04_coding_iv.py/05_star_repitition_v.py
@@ -0,0 +1,9 @@
+#get two words
+#W1 contains two parts - the first part contains w2 and the second part contains remaining letters
+W1 = input()
+W2 = input()
+length = len(W2)
+
+#print w1 with the first part as stars
+result = "*" * length + W1[l: ]
+print(result)
