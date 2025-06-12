6c2ddaa4a536838a0ca3dda42294c9770bfb7f3a
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-12 16:58:46 +0530

    Create 06_less_than_or_equal_ii.py

---
+++ 05_relational_operators/03_part_3/06_less_than_or_equal_ii.py
@@ -0,0 +1,12 @@
+#Reads two numbers A and B
+A = int(input())
+B = int(input())
+
+#Check if A is less than or equal to B
+C = A <= B 
+
+#Check if B is less than or equal to A
+D = B <= A 
+
+print("A <= B is " + str(C))
+print("B <= A is " + str(D))
