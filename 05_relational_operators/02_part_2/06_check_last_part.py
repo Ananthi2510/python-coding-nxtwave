215ca8790ca1b33f453ea67208b9090b49755409
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-12 15:55:43 +0530

    Create 06_check_last_part.py

---
+++ 05_relational_operators/02_part_2/06_check_last_part.py
@@ -0,0 +1,13 @@
+#Reads two words
+first_word = input()
+second_word = input()
+
+first_word_length = len(first_word)
+second_word_length = len(second_word)
+
+#Check if the second word is the last part of the first word 
+start_index = first_word_length - second_word_length
+part = first_word[start_index: ]
+
+result = part == second_word
+print(result)
