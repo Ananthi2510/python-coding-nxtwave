214877ad6a5cffa81edb1622315927205717059a
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-12 16:16:36 +0530

    Create 01_compare_digits.py

---
+++ 05_relational_operators/03_part_3/01_compare_digits.py
@@ -0,0 +1,11 @@
+#Reads a two digit number
+N = int(input())
+#Check the number is greater than 25
+print(N > 25)
+
+#Check if the first digit of N is greater than second digit of N
+N = str(N)
+A = int(N[0])
+B = int(N[1])
+
+print(A > B)
