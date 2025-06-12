a74267c705f503ba63fb20fab8b52aee80e2a52e
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-12 17:07:04 +0530

    Create 08_profit.py

---
+++ 05_relational_operators/03_part_3/08_profit.py
@@ -0,0 +1,7 @@
+#Reads selling price and buying price of a product
+selling_price = int(input())
+buying_price = int(input())
+
+#Check if selling price is greater than buying price
+profit = selling_price > buying_price
+print(profit)
