d4e47e2558187dd66588f0f744dc3475405234c9
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 14:51:02 +0530

    Create 04_not_negative.py

---
+++ 06_logical_operators/02_part_2/04_not_negative.py
@@ -0,0 +1,7 @@
+#Reads two numbers 
+first_number = int(input())
+second_number = int(input())
+
+#Check if both the numbers are not negative
+result = (first_number > 0) and (second_number > 0)
+print(result)
