c789fd65e45c97ad224136d6354c44695f42f274
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 14:12:49 +0530

    Create 03_positive_numbers.py

---
+++ 06_logical_operators/01_part_1/03_positive_numbers.py
@@ -0,0 +1,7 @@
+#Reads two numbers
+first_number = int(input())
+second_number = int(input())
+
+#Check if both the numbers are positive
+is_positive = (first_number > 0) and (second_number > 0)
+print(is_positive)
