cd4c588e5fa2df382401d265b1f0c0a1e8131316
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 14:08:36 +0530

    Create 01_greater_than_nine.py

---
+++ 06_logical_operators/01_part_1/01_greater_than_nine.py
@@ -0,0 +1,7 @@
+#Reads two numbers
+first_number = int(input())
+second_number = int(input())
+
+#Check if both the numbers are greater than 9
+result = (first_number > 9) and (second_number > 9)
+print(result)
