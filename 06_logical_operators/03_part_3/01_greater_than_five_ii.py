3f1cc4beaed7f1be731470d15fb4fa350c536f03
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 15:44:52 +0530

    Create 01_greater_than_five_ii.py

---
+++ 06_logical_operators/03_part_3/01_greater_than_five_ii.py
@@ -0,0 +1,8 @@
+#read three numbers
+first__number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+#checks if each of the given numbers is greater than 5
+result = (first_number > 5) and (second_number > 5) and (third_number > 5)
+print(result)
