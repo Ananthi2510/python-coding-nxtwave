2d4a547649fa482c2414c1d1b0da9ab393ac7433
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 15:48:03 +0530

    Create 02_greater_or_equal_20.py

---
+++ 06_logical_operators/03_part_3/02_greater_or_equal_20.py
@@ -0,0 +1,8 @@
+#read three numbers
+first__number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+#checks if each number is greater than or equal to 20
+result = (first_number >= 20) and (second_number >= 20) and (third_number >= 20)
+print(result)
