95a819339c3f91627082d83c2fde7076a3989c16
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 14:52:50 +0530

    Create 05_not_less_than_15.py

---
+++ 06_logical_operators/02_part_2/05_not_less_than_15.py
@@ -0,0 +1,7 @@
+#Reads two numbers
+first_number = int(input())
+second_number = int(input())
+
+#checks if any of the given numbers is not less than 15
+reuslt = (first_number >= 15) or (second_number >= 15)
+print(result)
