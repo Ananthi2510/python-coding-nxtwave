d294d2f916fa43ee1566016ee1e39a8ca1539f9b
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 14:45:56 +0530

    Create 02_same_string.py

---
+++ 06_logical_operators/02_part_2/02_same_string.py
@@ -0,0 +1,8 @@
+#Read three strings
+first_string = input()
+second_string = input()
+third_string = input()
+
+#checks if one of the string is equal to women
+result = (first_string == "women") or (second_string == "women") or (third_string == "women") 
+print(result)
