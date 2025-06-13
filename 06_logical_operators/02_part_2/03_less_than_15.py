6612fb4b98790a3cba307fca8768443bf67019a6
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-13 14:49:12 +0530

    Create 03_less_than_15.py

---
+++ 06_logical_operators/02_part_2/03_less_than_15.py
@@ -0,0 +1,7 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+#Check if any of the number is less than 15
+result = (first_number < 15) or (second_number < 15) or (third_number < 15)
+print(result)
