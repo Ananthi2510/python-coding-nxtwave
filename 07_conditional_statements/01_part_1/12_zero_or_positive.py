98aacf23c692dac2103e072651d4dc87d632e185
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-16 11:11:16 +0530

    Create 12_zero_or_positive.py

---
+++ 07_conditional_statements/01_part_1/12_zero_or_positive.py
@@ -0,0 +1,12 @@
+#Reads a number
+
+number = int(input())
+
+#checks if the given number is equal to 0 or positive. 
+#If the number is equal to 0,   print "Zero". 
+#If the number is positive (greater than 0), print "Positive"
+if number == 0:
+  
+    print("Zero")
+if number > 0:
+    print("Positive")
