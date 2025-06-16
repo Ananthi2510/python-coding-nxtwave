ecaf7e3140d5b8d580c6a25bba966a9b4b8f5fa8
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-16 10:58:04 +0530

    Create 07_square_or_rectangle.py

---
+++ 07_conditional_statements/01_part_1/07_square_or_rectangle.py
@@ -0,0 +1,9 @@
+#Given the length and breadth of a box
+length = int(input())
+breadth = int(input())
+
+#check if it is a square or rectangle
+if length == breadth:
+    print("Square")
+else:
+    print("Rectangle")
