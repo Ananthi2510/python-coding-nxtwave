4401fb37f2f1d373e0afdca29a2a1ee90403bac7
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-16 10:49:04 +0530

    Create 03_greatest_among_two_iii.py

---
+++ 07_conditional_statements/01_part_1/03_greatest_among_two_iii.py
@@ -0,0 +1,11 @@
+#reads two numbers
+first_number = int(input())
+second_number = int(input())
+
+#Prints the greatest among two numbers
+if first_number > second_number:
+    greater_number = first_number 
+else:
+    greater_number = second_number
+  
+print(greater_number)
