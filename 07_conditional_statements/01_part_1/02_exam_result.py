2804f1674a2460080d905143e37178c6557fefe4
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-16 10:46:59 +0530

    Create 02_exam_result.py

---
+++ 07_conditional_statements/01_part_1/02_exam_result.py
@@ -0,0 +1,8 @@
+#Reads students mark as input
+mark = int(input())
+
+#Print "PASS" if the student has scored more than 50, otherwise print "FAIL".
+if mark > 50:
+    print("PASS")
+else:
+    print("FAIL")
