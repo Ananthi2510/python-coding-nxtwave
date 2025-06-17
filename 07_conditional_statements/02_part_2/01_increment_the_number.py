f41b8b2e434b1bd68a87d72b5b8dcb3154e41c42
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-17 12:21:00 +0530

    Create 01_increment_the_number.py

---
+++ 07_conditional_statements/02_part_2/01_increment_the_number.py
@@ -0,0 +1,10 @@
+#Read a number 
+number = int(input())
+
+#checks if N is greater than 10. If N is greater than 10,  print the result of N + 5. Otherwise, print the result of N + 1.
+if number > 10:
+    number = number + 5
+else:
+    number = number + 1
+
+print(number)
