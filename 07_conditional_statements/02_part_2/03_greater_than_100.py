2f2ecf0d86e4ea71e93954e090156ab13bb6a539
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-17 12:31:14 +0530

    Create 03_greater_than_100.py

---
+++ 07_conditional_statements/02_part_2/03_greater_than_100.py
@@ -0,0 +1,10 @@
+#Reads three numbers
+A = int(input())
+B = int(input())
+C = int(input())
+
+#Check if each number is greater than 100
+if A > 100 and B > 100 and C > 100:
+    print("All are greater than 100")
+else:
+    print("Not all are greater than 100")
