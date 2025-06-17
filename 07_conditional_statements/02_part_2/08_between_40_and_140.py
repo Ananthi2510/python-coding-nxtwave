161edbec6a6a3da76eeb8b8eb22457175939c622
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-17 13:03:03 +0530

    Create 08_between_40_and_140.py

---
+++ 07_conditional_statements/02_part_2/08_between_40_and_140.py
@@ -0,0 +1,9 @@
+#Reads two numbers
+a = int(input())
+b = int(input())
+
+#Checks if any of the numbers is between 40 and 140
+if 40 < a < 140 or 40 < b < 140:
+    print("Between 40 and 140: Yes")
+else:
+    print("Between 40 and 140: No")
