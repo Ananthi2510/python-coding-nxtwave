aa056f17fb2d5aaaa4dc36a961a17cd5cb37f9fd
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-17 13:19:12 +0530

    Create 12_bonus_salary.py

---
+++ 07_conditional_statements/02_part_2/12_bonus_salary.py
@@ -0,0 +1,10 @@
+salary = int(input())
+experience = int(input())
+
+#check if the employee's years of service is more than 5. 
+#If it is, the employee is eligible for the bonus.
+#bonus ixss 5% of their salary.
+if experience > 5:
+    print(5 / 100 * s)
+else:
+    print("No Bonus")
