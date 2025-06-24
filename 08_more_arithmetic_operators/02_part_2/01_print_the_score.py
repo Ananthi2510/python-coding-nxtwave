00b33804a9f3313cd2dc31b6e19d01ea2d2173eb
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-24 14:49:28 +0530

    Create 01_print_the_score.py

---
+++ 08_more_arithmetic_operators/02_part_2/01_print_the_score.py
@@ -0,0 +1,13 @@
+#reads a distance D in kilometers and calculates the score based on the following rules:
+#If D is less than or equal to 10, the score is D.
+#If D is greater than 10, the score is the sum of 10 and (D - 10) * 3.
+
+distance = int(input())
+
+if distance <= 10:
+    score = distance
+else:
+    remaining_distance = distance - 10
+    score = 10 + (remaining_distance * 3)
+
+print(score)
