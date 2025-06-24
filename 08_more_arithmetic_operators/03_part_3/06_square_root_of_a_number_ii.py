3645fa8c5790403d318cf13f678104330d305ea3
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-06-24 18:31:27 +0530

    Create 06_square_root_of_a_number_ii.py

---
+++ 08_more_arithmetic_operators/03_part_3/06_square_root_of_a_number_ii.py
@@ -0,0 +1,9 @@
+#Reads two numbers
+#Checks if the square root of A is equal to B 
+
+A = int(input())
+B = int(input())
+if A ** 0.5 == B:
+    print("Square root of A is equal to B")
+else:
+    print("Square root of A is not equal to B")
