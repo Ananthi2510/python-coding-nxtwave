c4137a0a6da8727bf73a4cf6d83e8811e613f2e7
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-07-02 12:58:12 +0530

    Create 05_weather_condition.py

---
+++ 09_nested_conditional_statements/02_part_2/05_weather_condition.py
@@ -0,0 +1,16 @@
+#Display a customized message based on the given temperature
+
+T = float(input())
+
+if T < 0:
+    print("Freezing weather")
+elif 0 <= T < 10:
+    print("Very Cold weather")
+elif 10 <= T < 20:
+    print("Cold weather")
+elif 20 <= T < 30:
+    print("Normal")
+elif 30 <= T < 40:
+    print("Hot")
+elif T >= 40:
+    print("Very Hot")
