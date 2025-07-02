401a98cc2aac4467cae0e28637124448ca7c80ea
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-07-02 12:59:30 +0530

    Create 06_relation_betwwen_two_numbers.py

---
+++ 09_nested_conditional_statements/02_part_2/06_relation_betwwen_two_numbers.py
@@ -0,0 +1,11 @@
+#to print the relation  of numbers A and B
+
+A = int(input())
+B = int(input())
+
+if A == B:
+    print("A == B")
+elif A > B:
+    print("A > B")
+elif A < B:
+    print("A < B")
