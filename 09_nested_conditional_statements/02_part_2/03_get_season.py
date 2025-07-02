fa076dfeb0e64e90e1e0d6dcce6f2ac9d036a383
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-07-02 12:56:00 +0530

    Create 03_get_season.py

---
+++ 09_nested_conditional_statements/02_part_2/03_get_season.py
@@ -0,0 +1,14 @@
+#To find the season of the given month number
+
+month = int(input())
+
+if month == 11 or month == 12 or month == 1:
+    print("Winter")
+elif month == 2 or month == 3:
+    print("Spring")
+elif month == 4 or month == 5 or month == 6:
+    print("Summer")
+elif month == 7 or month == 8:
+    print("Rainy")
+elif month == 9 or month == 10:
+    print("Autumn")
