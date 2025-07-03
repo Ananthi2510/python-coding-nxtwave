5b24907b8551e0e5c6d3de1486a72e214d72329b
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-07-03 18:00:08 +0530

    Create 02_find_the_grout_ii.py

---
+++ 09_nested_conditional_statements/04_part_4.py/02_find_the_grout_ii.py
@@ -0,0 +1,17 @@
+
+
+number = int(input())
+remainder = number % 6
+
+if remainder == 1:
+    print("Group 1")
+elif remainder == 2:
+    print("Group 2")
+elif remainder == 3:
+    print("Group 3")
+elif remainder == 4:
+    print("Group 4")
+elif remainder == 5:
+    print("Group 5")
+else:
+    print("Group 6")
