3fe81e4145ef5c6466fc1f2e4b2d4acea853cd32
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-07-11 14:24:19 +0530

    Create 08_ten_numbers_after_N.py

---
+++ 10_while_loops/01_part_1/08_ten_numbers_after_N.py
@@ -0,0 +1,10 @@
+#reads a number N and prints the 10 numbers after N. Each number should be printed on a new line.
+
+number = int(input())
+
+counter = 0
+ 
+while counter < 10:  
+    counter = counter + 1
+    number = number + 1  
+    print(number)
