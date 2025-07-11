3715e533fa7ce0ba79be8ae469bdf48adfb2684e
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-07-11 14:21:17 +0530

    Create 06_cubes_of_N_numbers.py

---
+++ 10_while_loops/01_part_1/06_cubes_of_N_numbers.py
@@ -0,0 +1,9 @@
+# reads a number N and prints the cube of N numbers starting from 1
+
+number = int(input())
+
+counter = 0
+
+while counter < number:
+    counter = counter + 1
+    print(counter ** 3)
