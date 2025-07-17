3d21c6851357fb17bae789b26245a9fc740725b3
Ananthi K <ananthikalishwaran2002@gmail.com>
2025-07-17 18:59:55 +0530

    Create 04_right_angled_triangle.py

---
+++ 10_while_loops/03_part_3/04_right_angled_triangle.py
@@ -0,0 +1,13 @@
+#Reads a number N and print two Right angled triangles each of N rows using stars(*)
+
+N = int(input())
+counter = 1 
+
+while counter <= N:
+    print("* " * counter)
+    counter = counter + 1
+    
+counter = 1 
+while counter <= N:
+    print("* " * counter)
+    counter = counter + 1 
